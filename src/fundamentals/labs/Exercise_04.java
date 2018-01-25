79998cedcd2ed9d29b157a6b29b7b4ed0767e9c5
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-01-25 13:11:45 -0700

    lecture 1 and 2 labs with java book source included

---
+++ src/fundamentals/labs/Exercise_04.java
@@ -0,0 +1,18 @@
+package fundamentals.labs;
+
+/**
+ * Fundamentals Exercise 4: Area and Perimeter
+ *
+ *      Write the necessary code to calculate the area and perimeter of a cylinder
+ *      with a radius of 3.14 and a height of 5. Print out the result.
+ *
+ */
+
+public class Exercise_04 {
+
+    public static void main(String[] args) {
+
+        // write code here
+
+    }
+}
---
+++ src/interfaces/examples/ExtBook.java
@@ -0,0 +1,28 @@
+package interfaces.examples;
+
+class ExtBook extends main.java.packages.interfaces.bookpack.Book3 {
+    private String publisher;
+
+    public ExtBook(String t, String a, int d, String p) {
+        super(t, a, d);
+        publisher = p;
+    }
+
+    public void show() {
+        super.show();
+        System.out.println(publisher);
+        System.out.println();
+    }
+
+    public String getPublisher() { return publisher; }
+    public void setPublisher(String p) { publisher = p; }
+
+    /* These are OK because subclass can access
+       a protected member. */
+    public String getTitle() { return title; }
+    public void setTitle(String t) { title = t; }
+    public String getAuthor() { return author; }
+    public void setAuthor(String a) { author = a; }
+    public int getPubDate() { return pubDate; }
+    public void setPubDate(int d) { pubDate = d; }
+}
