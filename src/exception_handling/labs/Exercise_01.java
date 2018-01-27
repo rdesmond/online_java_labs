e08e28a60f7ebd5c8cd64b120c341102701f54f8
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-01-26 17:47:29 -0700

    lecture 5 and 6 part1

---
+++ src/exception_handling/labs/Exercise_01.java
@@ -0,0 +1,14 @@
+package exception_handling.labs;
+
+/**
+ * Exception Handling Exercise 1:
+ *
+ *      1) Demonstrate a try/catch.
+ *      2) Demonstrate a try/catch with multiple catch statements.
+ *      3) Demonstrate a try/catch/finally block.
+ *      4) Demonstrate a nested try/catch.
+ *      5) Demonstrate how to throw an exception.
+ *      6) Demonstrate throwing an exception in one method and catching it in another method.
+ *
+ */
+
---
+++ src/interfaces/labs/Exercise_01.java
@@ -0,0 +1,12 @@
+package interfaces.labs;
+
+/**
+ * Interfaces Exercise 1:
+ *
+ *      1) Write an interface with at least three methods. Write two classes that implement the
+ *          interface in different ways.
+ *      2) Write a class that extends one of the classes from the previous step. Overwrite the methods in
+ *          the parent class.
+ */
+
+
