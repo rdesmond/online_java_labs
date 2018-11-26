fadc0e1e1dc2ae6a6a1a6b3f07a9345a35ece481
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-11-26 11:37:45 +0100

    exercise_04 name change

--- src/fundamentals/labs/Exercise_04.java
+++ src/fundamentals/labs/Exercise_04.java
@@ -1,7 +1,7 @@
 package fundamentals.labs;
 
 /**
- * Fundamentals Exercise 4: Area and Perimeter
+ * Fundamentals Exercise 4: Volume and Surface Area
  *
  *      Write the necessary code to calculate the volume and surface area of a cylinder
  *      with a radius of 3.14 and a height of 5. Print out the result.
